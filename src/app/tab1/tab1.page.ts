e0ff3619ba9560932f7e422d451b86b4f7093ada
elvisoft <elvisoftbeta@gmail.com>
2019-12-04 19:37:29 -0300

    Primera subida

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,36 @@ export class Tab1Page {
 
   constructor() {}
 
+  instrumentos = [
+    {
+      nombre: 'flauta',
+      imagen: 'assets/imagenes/flauta.png',
+      audio: 'assets/sonidos/flauta.mp3'
+    },
+    {
+      nombre: 'bombo1',
+      imagen: 'assets/imagenes/bombo1.png',
+      audio: 'assets/sonidos/bombo1.mp3'
+    },
+    {
+      nombre: 'bombo2',
+      imagen: 'assets/imagenes/bombo2.png',
+      audio: 'assets/sonidos/bombo2.mp3'
+    },
+    {
+      nombre: 'Platillos',
+      imagen: 'assets/imagenes/platillos.png',
+      audio: 'assets/sonidos/platillos.mp3'
+    }
+  ]
+
+  reproducirSonido(instrumento){
+    let sonido = new Audio();
+    sonido.src=instrumento.audio;
+    sonido.load();
+    sonido.play();
+  }
+
+  
+
 }
